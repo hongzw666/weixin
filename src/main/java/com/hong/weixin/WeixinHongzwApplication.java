a3646d59a33d2957f1af3a8c330e8f735774be07
hongzw666 <642546103@qq.com>
2019-03-17 18:24:23 +0800

    第二次提交

---
+++ src/main/java/com/hong/weixin/WeixinHongzwApplication.java
@@ -0,0 +1,13 @@
+package com.hong.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinHongzwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinHongzwApplication.class, args);
+	}
+
+}
