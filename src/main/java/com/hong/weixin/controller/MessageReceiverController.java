cea242ea4e2ea9eb537b11064f159ebf3527beba
hongzw666 <642546103@qq.com>
2019-03-28 19:33:59 +0800

    修改

--- src/main/java/com/hong/weixin/controller/MessageReceiverController.java
+++ src/main/java/com/hong/weixin/controller/MessageReceiverController.java
@@ -3,6 +3,7 @@ package com.hong.weixin.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,7 +36,7 @@ public class MessageReceiverController {
 		System.out.println("hello kemao!");
 		return echostr;
 	}
-	
+	@PostMapping
 	public String onMessage(
 			@RequestParam("signature") String signature, //
 			@RequestParam("timestamp") String timestamp, //
