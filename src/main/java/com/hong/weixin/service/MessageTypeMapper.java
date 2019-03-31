da5ae56063d322df1955443e5936388491790d25
hongzw666 <642546103@qq.com>
2019-03-31 18:48:12 +0800

    修修

--- src/main/java/com/hong/weixin/service/MessageTypeMapper.java
+++ src/main/java/com/hong/weixin/service/MessageTypeMapper.java
@@ -14,6 +14,7 @@ import com.hong.domain.voice.VoiceInMessage;
 
 public class MessageTypeMapper {
 	private static Map<String,Class<? extends InMessage>> typeMap = new ConcurrentHashMap<>();
+	// 通过一个Map记录了消息类型和类的关系
 	static {
 		typeMap.put("text", TextInMessage.class);
 		typeMap.put("image", ImageInMessage.class);
@@ -23,6 +24,8 @@ public class MessageTypeMapper {
 		typeMap.put("location", LocationInMessage.class);
 		typeMap.put("link", LinkInMessage.class);
 	}
+	// 通过消息类型获取对应的类
+	@SuppressWarnings("unchecked")
 	public static <T extends InMessage> Class<T> getClass (String type){
 		
 		return (Class<T>) typeMap.get(type);	
